b7b04be023f1edace4fab137d2b399eb94b99f65
Cacow69 <cmfcursino@gmail.com>
2026-05-04 19:30:51 -0300

    commit inicial (estrutura base)

---
+++ src/components/Header.tsx
@@ -0,0 +1,28 @@
+import Logo from "./Logo"
+import { NavLink } from "react-router-dom"
+import { User } from "lucide-react"
+
+function Header() { 
+
+  return (
+    <header className='grid grid-cols-3 w-full bg-[var(--azul-escuro)] h-[80px] justify-center items-center relative'>
+      <div className="flex justify-center items-center gap-12">
+        <NavLink to="#" className="text-sm text-[var(--branco)] duration-200 font-semibold hover:text-[var(--azul)] transition-all">Aeronaves</NavLink>
+        <NavLink to="#" className="text-sm text-[var(--branco)] duration-200 font-semibold hover:text-[var(--azul)] transition-all">Peças</NavLink>
+        <NavLink to="#" className="text-sm text-[var(--branco)] duration-200 font-semibold hover:text-[var(--azul)] transition-all">Aeronaves</NavLink>
+      </div>
+
+      <div className="flex justify-center items-center">
+        <Logo />
+      </div>
+
+      <div className="flex justify-center items-center gap-12">
+        <NavLink to="#" className="text-sm text-[var(--branco)] duration-200 font-semibold hover:text-[var(--azul)] transition-all">Funcionários</NavLink>
+        <User className="text-[var(--branco)] cursor-pointer" />
+      </div>
+    </header>
+  )
+}
+
+
+export default Header
